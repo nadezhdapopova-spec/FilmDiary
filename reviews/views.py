7dc489739002609d1335ae0f5c6db465929e44b4
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-01-20 03:05:34 +0700

    modified WatchedListView and ReviewsListView, fixed reviews.html and review_card.html

--- reviews/views.py
+++ reviews/views.py
@@ -1,44 +1,65 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
 from django.db import models
-from django.http import Http404
+from django.db.models import OuterRef, Exists
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from films.models import Film
+from films.models import Film, UserFilm
 from services.permissions import can_user_view, can_user_edit, can_user_delete
 from reviews.forms import ReviewForm
 from reviews.models import Review
 
 
-class WatchedListView(LoginRequiredMixin, ListView):
-    """Представление для отображения просмотренных=оцененных фильмов"""
-
+class BaseReviewListView(LoginRequiredMixin, ListView):
+    """Юазовый класс для списков просмотренных фильмов"""
     model = Review
-    template_name = "reviews/reviews.html"
     context_object_name = "reviews"
     paginate_by = 12
+
+    def get_base_queryset(self):
+        """Базовый queryset с annotate для карточек"""
+        qs = (
+            Review.objects
+            .filter(user=self.request.user)
+            .select_related("film")
+            .order_by("-updated_at")
+        )
+
+        user_films = UserFilm.objects.filter(
+            user=self.request.user,
+            film=OuterRef("film")
+        )
+
+        return qs.annotate(
+            is_favorite=Exists(user_films.filter(is_favorite=True)),
+            is_planned=Exists(user_films.filter(is_planned=True))
+        )
+
+
+class WatchedListView(BaseReviewListView):
+    """Представление для отображения просмотренных=оцененных фильмов"""
+
+    template_name = "reviews/reviews.html"
+
 
     def get_context_data(self, **kwargs):
         """Добавляет поиск по просмотренному=оцененному в контекст"""
         context = super().get_context_data(**kwargs)
         query = self.request.GET.get("q", "").strip()
 
-        context["search_type"] = "watched"
-        context["query"] = query
-        context["params"] = f"&q={query}&source=watched" if query else "&source=watched"
-        context["template"] = "reviews"
-
+        context.update({
+            "search_type": "watched",
+            "query": query,
+            "params": f"&q={query}&source=watched" if query else "&source=watched",
+            "template": "reviews",
+        })
         return context
 
 
     def get_queryset(self):
         """Возвращает список просмотренных=оцененных фильмов пользователя"""
-        queryset = (Review.objects
-                    .filter(user=self.request.user)
-                    .select_related("film", "user")
-                    .order_by("-updated_at"))
+        queryset = self.get_base_queryset()
 
         query = self.request.GET.get("q", "").strip()
         if query:
@@ -48,18 +69,14 @@ class WatchedListView(LoginRequiredMixin, ListView):
 
 class ReviewsListView(WatchedListView):
     """Представление для отображения списка текстовых отзывов на фильмы"""
-    model = Review
-    context_object_name = "reviews"
+
     template_name = "reviews/reviews.html"
-    paginate_by = 12
 
     def get_queryset(self):
         """Возвращает список отзывов пользователя, осуществляет поиск по q"""
-        queryset = (Review.objects
-                    .filter(user=self.request.user, review__isnull=False)
+        queryset = (self.get_base_queryset().filter(review__isnull=False)
                     .exclude(review__exact="")
-                    .select_related("film", "user")
-                    .order_by("-updated_at"))
+                    .select_related("user"))
 
         query = self.request.GET.get("q", "").strip()
         if query:
@@ -67,7 +84,6 @@ class ReviewsListView(WatchedListView):
                        models.Q(film__title__icontains=query) |
                        models.Q(user__email__icontains=query)
                        )
-
         return queryset
 
     def get_context_data(self, **kwargs):
@@ -75,11 +91,12 @@ class ReviewsListView(WatchedListView):
         context = super().get_context_data(**kwargs)
         query = self.request.GET.get("q", "").strip()
 
-        context["search_type"] = "reviewed"
-        context["query"] = query
-        context["params"] = f"&q={query}&source=reviewed" if query else "&source=reviewed"
-        context["template"] = "reviews"
-
+        context.update({
+            "search_type": "reviewed",
+            "query": query,
+            "params": f"&q={query}&source=reviewed" if query else "&source=reviewed",
+            "template": "reviews",
+        })
         return context
 
 
