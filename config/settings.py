df75fcc24fc93a7507b1c40db354e3725e803935
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-02-03 22:49:10 +0700

    created deckerfile, docker-compose.yaml, github/workflows/ci_cd.yaml

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": "127.0.0.1",
+        "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
         "CONN_MAX_AGE": 0,  # закрываем после КАЖДОГО запроса, каждое соединение свежее
         "OPTIONS": {
