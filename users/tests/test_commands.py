c31f293cc388a798421bf6665a9497917ca27897
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-02-04 17:33:26 +0700

    fixed test_create_manager_command

--- users/tests/test_commands.py
+++ users/tests/test_commands.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management import call_command
 
 import pytest
@@ -6,7 +8,19 @@ from users.models import CustomUser
 
 
 @pytest.mark.django_db
-def test_create_manager_command():
+def test_create_manager_command(mocker):
     """Тестирует кастомную команду для создания группы Менеджер"""
+    mocker.patch.dict(
+        os.environ,
+        {
+            "MANAGER_EMAIL": "manager@test.com",
+            "MANAGER_PASSWORD": "testpass123",
+        },
+    )
+
     call_command("create_manager")
-    assert CustomUser.objects.filter(username="Admin_middle").exists()
+
+    manager = CustomUser.objects.get(username="Admin_middle")
+    assert manager.email == "manager@test.com"
+    assert manager.is_staff
+    assert manager.groups.filter(name="Manager").exists()
